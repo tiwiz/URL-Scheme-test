c0c6de67ff2f9886254b1610e9232a62b8fffb03
tiwiz <roberto.orgiu@gmail.com>
2014-02-04 15:17:30 +0100

    Initial release

---
+++ SchemeTest/src/main/java/it/androidworld/schemetest/MainActivity.java
@@ -0,0 +1,83 @@
+package it.androidworld.schemetest;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        Uri uri = getIntent().getData();
+        getInputParameter(uri);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+    private void getInputParameter(Uri uri){
+
+        if(null == uri) return;
+
+        String notizia = uri.getQueryParameter(getString(R.string.parameter));
+
+        if(null != notizia){
+            String messaggio = getString(R.string.toast,notizia);
+            Toast.makeText(this, messaggio, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+}
